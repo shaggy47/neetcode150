0cf5b2034e30714bddbf68654a68042ac5d0cd25
shaggy47 <vaibhavpfatkal@gmail.com>
2024-07-09 19:20:53 +0530

    Height of max area

---
+++ src/two-pointers/HeightOfMaxAreaProblem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace two_pointers
+{
+    internal class HeightOfMaxAreaProblem
+    {
+        public int MaxArea(int[] heights)
+        {
+
+            var (l, r) = (0, heights.Length - 1);
+            int maxArea = int.MinValue;
+
+            while (l < r)
+            {
+                var area = Math.Min(heights[l], heights[r]) * (r - l);
+                maxArea = Math.Max(area, maxArea);
+
+                if (heights[l] < heights[r])
+                    l++;
+                else
+                    r--;
+            }
+
+            return maxArea;
+        }
+    }
+}
